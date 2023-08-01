3b2fc6ff261f0fd1e68dcdf1e67f8b899413847f
Bryce Bryce <bpearce@angelo.edu>
2023-08-01 16:37:02 -0700

    further config changes

--- src/types/typeIndex.d.ts
+++
@@ -1,7 +0,0 @@
-import type { FlatfileWorkbook, FormattedRecordData, InsertRecordResponse, WorkbookResponse } from './Flatfile.ts';
-export type {
-    FlatfileWorkbook,
-    FormattedRecordData,
-    WorkbookResponse,
-    InsertRecordResponse
-};
