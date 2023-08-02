c75eb5524b9fba807caa4d98b04e1ab3290ab467
Bryce Bryce <bpearce@angelo.edu>
2023-08-01 19:31:34 -0700

    attempt to fix exports

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ import { formatCsvToPublicRecord } from './helpers/helpers.js';
 import type { ParseCsv } from './types/ParseCsv.js';
 import type { FlatfileWorkbook, FormattedRecordData } from './types/Flatfile.js';
 
-export const convertCsvToWorkbook = ({ actions, columnHeaders, csv, fieldKeys, workbookEnvironmentId, workbookSpaceId, fieldTypes, sheetAccess, sheetName, slugName, workbookName, hasColumnHeaders = false, options = {
+const convertCsvToWorkbook = ({ actions, columnHeaders, csv, fieldKeys, workbookEnvironmentId, workbookSpaceId, fieldTypes, sheetAccess, sheetName, slugName, workbookName, hasColumnHeaders = false, options = {
     escapeCharacter: '\\',
     ltrim: true,
     rtrim: true
@@ -24,7 +24,7 @@ export const convertCsvToWorkbook = ({ actions, columnHeaders, csv, fieldKeys, w
     return { workbook, recordData: formattedRecords };
 }
 
-export const createFlatfile = async ({ workbook, recordData, flatfileApiKey, }: { workbook: FlatfileWorkbook, recordData: FormattedRecordData[][], flatfileApiKey: string }) => {
+const createFlatfile = async ({ workbook, recordData, flatfileApiKey, }: { workbook: FlatfileWorkbook, recordData: FormattedRecordData[][], flatfileApiKey: string }) => {
     // create the workbook
     const workbookResponse = await createWorkbook(workbook, flatfileApiKey);
     const sheetId = workbookResponse.data.sheets[0].id;
